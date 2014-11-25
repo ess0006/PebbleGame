57c19ac97e1f664ccaef95a92aac41695eab6b47
Mpritch11 <Mpritch11@gmail.com>
2014-11-25 15:31:40 -0600

    Filled out most of the Heuristic class.

--- src/heuristics/heuristic.py
+++ src/heuristics/heuristic.py
@@ -1,7 +1,9 @@
 """
 Created on Nov 25, 2014
 
-@author: mpritchard
+@author: Michael Pritchard
+@author: Eric Shaw
+
 """
 
 
@@ -11,5 +13,44 @@ class Heuristic(object):
 
     """
 
-    def __init__(self):
-        """ Constructor. """
+    def __init__(self, rows=2, row_buckets=2):
+        """
+        Constructor.
+
+        @param: rows
+        @param row_buckets
+
+        """
+        self.validate_rows(rows)
+        self.validate_row_buckets(row_buckets)
+        self.rows = rows
+        self.row_buckets = row_buckets
+
+    def validate_rows(self, rows):
+        """ Performs type and value checking for the rows parameter """
+        if not isinstance(rows, int):
+            raise TypeError("heuristic.Heuristic: expected <type \'int\'> " +
+                            "for \'rows\', found " + type(rows))
+        if rows < 2:
+            raise ValueError("heuristic.Heuristic: \'rows\' must have a " +
+                             "positive value greater than 1.")
+
+    def validate_row_buckets(self, row_buckets):
+        """ Performs type and value checking for the row_buckets paramter. """
+        if not isinstance(row_buckets, int):
+            raise TypeError("heuristic.Heuristic: expected <type \'int\'> " +
+                            "for \'row_buckets\', found " + type(row_buckets))
+        if row_buckets < 2:
+            raise ValueError("heuristic.Heuristic: \'row_buckets\' must have" +
+                             " a positive value greater than 1.")
+
+    def set_rows(self, rows):
+        self.validate_rows(rows)
+        self.rows = rows
+
+    def set_row_buckets(self, row_buckets):
+        self.validate_row_buckets(row_buckets)
+        self.row_buckets = row_buckets
+
+    def evaluate_board_state(self, board_state):
+        pass
